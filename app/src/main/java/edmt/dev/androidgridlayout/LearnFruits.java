3ab2e44c62c15fd57ca9596d45bd4ed46409014d
Mirou <amarhamlaoui@gmail.com>
2020-08-22 00:46:04 +0100

    - Fixed all warnings except the AsyncTask warning.

--- app/src/main/java/edmt/dev/androidgridlayout/LearnFruits.java
+++ app/src/main/java/edmt/dev/androidgridlayout/LearnFruits.java
@@ -11,18 +11,21 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class LearnFruits extends AppCompatActivity {
-    boolean playing = false;
-    TextToSpeech t2s;
-    String text;
-    ImageView play;
+    private boolean playing = false;
+    private TextToSpeech t2s;
+    private String text;
+    private List<Thing> fruits;
+    private ImageView play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,48 +44,8 @@ public class LearnFruits extends AppCompatActivity {
         }
         play = findViewById(R.id.play);
         ViewPager2 fruitViewPager = findViewById(R.id.viewPager);
-        List<Thing> fruits = new ArrayList<>();
-        fruits.add(new Thing(getString(R.string.apple),"la",R.drawable.apple));
-        fruits.add(new Thing(getString(R.string.banana),"la",R.drawable.banana));
-        fruits.add(new Thing(getString(R.string.berries),"la",R.drawable.berries));
-        fruits.add(new Thing(getString(R.string.cherry),"la",R.drawable.cherry));
-        fruits.add(new Thing(getString(R.string.grapes),"le",R.drawable.grapes));
-        fruits.add(new Thing(getString(R.string.coconut),"la",R.drawable.coconut));
-        fruits.add(new Thing(getString(R.string.kiwi),"le",R.drawable.kiwi));
-        fruits.add(new Thing(getString(R.string.lemon),"le",R.drawable.lemon));
-        fruits.add(new Thing(getString(R.string.melon),"le",R.drawable.melon));
-        fruits.add(new Thing(getString(R.string.orange),"l'",R.drawable.orange));
-        fruits.add(new Thing(getString(R.string.pineapple),"l'",R.drawable.pineapple));
-        fruits.add(new Thing(getString(R.string.strawberry),"la",R.drawable.strawberry));
-        fruits.add(new Thing(getString(R.string.watermelon),"la",R.drawable.watermelon));
-        fruits.add(new Thing(getString(R.string.pumpkin),"la",R.drawable.pumpkin));
-        fruits.add(new Thing(getString(R.string.tomato),"la",R.drawable.tomato));
-        fruits.add(new Thing(getString(R.string.grenade),"la",R.drawable.pomegranate));
-        fruits.add(new Thing(getString(R.string.apricot),"l'",R.drawable.apricot));
-        fruits.add(new Thing(getString(R.string.avocado),"l'",R.drawable.avocado));
-        fruits.add(new Thing(getString(R.string.blueberry),"la",R.drawable.blueberry));
-        fruits.add(new Thing(getString(R.string.chestnut),"la",R.drawable.chestnut));
-        fruits.add(new Thing(getString(R.string.fig),"la",R.drawable.fig));
-        fruits.add(new Thing(getString(R.string.gooseberry),"la",R.drawable.gooseberry));
-        fruits.add(new Thing(getString(R.string.grapefruit),"le",R.drawable.grapefruit));
-        fruits.add(new Thing(getString(R.string.guava),"la",R.drawable.guava));
-        fruits.add(new Thing(getString(R.string.kumquat),"le",R.drawable.kumquat));
-        fruits.add(new Thing(getString(R.string.lime),"le",R.drawable.lime));
-        fruits.add(new Thing(getString(R.string.mango),"la",R.drawable.mango));
-        fruits.add(new Thing(getString(R.string.mangosteen),"le",R.drawable.mangosteen));
-        fruits.add(new Thing(getString(R.string.papaya),"la",R.drawable.papaya));
-        fruits.add(new Thing(getString(R.string.granadilla),"la",R.drawable.passion_fruit_1));
-        fruits.add(new Thing(getString(R.string.peacj),"la",R.drawable.peach));
-        fruits.add(new Thing(getString(R.string.pear),"la",R.drawable.pear));
-        fruits.add(new Thing(getString(R.string.persimmon),"le",R.drawable.persimmon));
-        fruits.add(new Thing(getString(R.string.plum),"la",R.drawable.plum));
-        fruits.add(new Thing(getString(R.string.quince),"le",R.drawable.quince));
-        fruits.add(new Thing(getString(R.string.rambutan),"le",R.drawable.rambutan));
-        fruits.add(new Thing(getString(R.string.raspberry),"la",R.drawable.raspberry));
-        fruits.add(new Thing(getString(R.string.date),"la",R.drawable.date));
-
-
-
+        fruits = new ArrayList<>();
+        addFruits();
         //Resources recources = getResources();
         //View view = findViewById(R.id.learn_layout);
         fruitViewPager.setAdapter(new Adapter(fruits));
@@ -114,10 +77,12 @@ public class LearnFruits extends AppCompatActivity {
 
                         @Override
                         public void onError(String utteranceId) {
+                            Log.e("progressListener","Error");
                         }
 
                         @Override
                         public void onStart(String utteranceId) {
+                            Log.e("progressListener","Started");
                         }
                     });
                     switch(language){
@@ -127,6 +92,10 @@ public class LearnFruits extends AppCompatActivity {
                         }
                         case "French":{
                             t2s.setLanguage(Locale.FRENCH);
+                            break;
+                        }
+                        default: {
+                            Toast.makeText(LearnFruits.this, "Arabic", Toast.LENGTH_SHORT).show();
                             break;
                         }
                     }
@@ -171,5 +140,46 @@ public class LearnFruits extends AppCompatActivity {
             play.setImageResource(R.drawable.play_button);
             playing = false;
         }
+    }
+
+    private void addFruits(){
+        fruits.add(new Thing(getString(R.string.apple),"la",R.drawable.apple));
+        fruits.add(new Thing(getString(R.string.banana),"la",R.drawable.banana));
+        fruits.add(new Thing(getString(R.string.berries),"la",R.drawable.berries));
+        fruits.add(new Thing(getString(R.string.cherry),"la",R.drawable.cherry));
+        fruits.add(new Thing(getString(R.string.grapes),"le",R.drawable.grapes));
+        fruits.add(new Thing(getString(R.string.coconut),"la",R.drawable.coconut));
+        fruits.add(new Thing(getString(R.string.kiwi),"le",R.drawable.kiwi));
+        fruits.add(new Thing(getString(R.string.lemon),"le",R.drawable.lemon));
+        fruits.add(new Thing(getString(R.string.melon),"le",R.drawable.melon));
+        fruits.add(new Thing(getString(R.string.orange),"l'",R.drawable.orange));
+        fruits.add(new Thing(getString(R.string.pineapple),"l'",R.drawable.pineapple));
+        fruits.add(new Thing(getString(R.string.strawberry),"la",R.drawable.strawberry));
+        fruits.add(new Thing(getString(R.string.watermelon),"la",R.drawable.watermelon));
+        fruits.add(new Thing(getString(R.string.pumpkin),"la",R.drawable.pumpkin));
+        fruits.add(new Thing(getString(R.string.tomato),"la",R.drawable.tomato));
+        fruits.add(new Thing(getString(R.string.grenade),"la",R.drawable.pomegranate));
+        fruits.add(new Thing(getString(R.string.apricot),"l'",R.drawable.apricot));
+        fruits.add(new Thing(getString(R.string.avocado),"l'",R.drawable.avocado));
+        fruits.add(new Thing(getString(R.string.blueberry),"la",R.drawable.blueberry));
+        fruits.add(new Thing(getString(R.string.chestnut),"la",R.drawable.chestnut));
+        fruits.add(new Thing(getString(R.string.fig),"la",R.drawable.fig));
+        fruits.add(new Thing(getString(R.string.gooseberry),"la",R.drawable.gooseberry));
+        fruits.add(new Thing(getString(R.string.grapefruit),"le",R.drawable.grapefruit));
+        fruits.add(new Thing(getString(R.string.guava),"la",R.drawable.guava));
+        fruits.add(new Thing(getString(R.string.kumquat),"le",R.drawable.kumquat));
+        fruits.add(new Thing(getString(R.string.lime),"le",R.drawable.lime));
+        fruits.add(new Thing(getString(R.string.mango),"la",R.drawable.mango));
+        fruits.add(new Thing(getString(R.string.mangosteen),"le",R.drawable.mangosteen));
+        fruits.add(new Thing(getString(R.string.papaya),"la",R.drawable.papaya));
+        fruits.add(new Thing(getString(R.string.granadilla),"la",R.drawable.passion_fruit_1));
+        fruits.add(new Thing(getString(R.string.peacj),"la",R.drawable.peach));
+        fruits.add(new Thing(getString(R.string.pear),"la",R.drawable.pear));
+        fruits.add(new Thing(getString(R.string.persimmon),"le",R.drawable.persimmon));
+        fruits.add(new Thing(getString(R.string.plum),"la",R.drawable.plum));
+        fruits.add(new Thing(getString(R.string.quince),"le",R.drawable.quince));
+        fruits.add(new Thing(getString(R.string.rambutan),"le",R.drawable.rambutan));
+        fruits.add(new Thing(getString(R.string.raspberry),"la",R.drawable.raspberry));
+        fruits.add(new Thing(getString(R.string.date),"la",R.drawable.date));
     }
 }
